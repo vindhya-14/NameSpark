56c7b705622a008b3ce76452ed2d0776e4c9bffc
Vindhya <vindhya3321@gmail.com>
2025-07-06 16:21:56 +0530

    added base url

--- src/api/teamApi.js
+++ src/api/teamApi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
 export const fetchTeamNames = async (prompt) => {
-  const res = await axios.post("http://localhost:5000/api/team/generate", {
+  const res = await axios.post(`${API_BASE}/api/team/generate`, {
     prompt,
   });
   return res.data.names;
